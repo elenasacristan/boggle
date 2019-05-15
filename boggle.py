1c1e7a4d9f2452df563753791687b38993d400c7
Elena Sacristan <elenitta1983@yahoo.es>
2019-05-15 21:37:43 +0000

    final commit

--- boggle.py
+++ boggle.py
@@ -79,9 +79,25 @@ def search(grid, dictionary):
         words.append(path_to_word(grid, path))
     return set(words)
     
-def get_dictionary(dictionary_file)
+def get_dictionary(dictionary_file):
     """
     Load Dictionary file
     """
     with open(dictionary_file) as f:
-        return [w.strip().upper() from w in f]
+        return [w.strip().upper() for w in f]
+        
+
+def main():
+    """
+    This is the function that will run the whole project
+    """
+    
+    grid = make_grid(3,3)
+    dictionary = get_dictionary("words.txt")
+    words = search(grid, dictionary)
+    for word in words:
+        print(word)
+    print("Found %s words" %len(words))
+    
+if __name__ == "__main__":
+    main()
